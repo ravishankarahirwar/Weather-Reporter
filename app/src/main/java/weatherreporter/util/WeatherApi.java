5e81725f3ee6c02a38c300711dc0cb66845df1d3
rrss_1005 <ravishankar.ahirwar@gmail.com>
2015-01-12 00:15:32 +0530

    Weather API files added

---
+++ app/src/main/java/weatherreporter/util/WeatherApi.java
@@ -0,0 +1,9 @@
+package weatherreporter.util;
+
+/**
+ * Created by acer on 1/12/2015.
+ */
+public class WeatherApi {
+    public static final String WEATHER="http://api.openweathermap.org/data/2.5/weather?";
+    public static final String  FORECAST="http://api.openweathermap.org/data/2.5/forecast/daily?";
+}
